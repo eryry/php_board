20eeceac667c4f9e47c1e92948d5981da6ed9bac
eryry <63179394+eryry@users.noreply.github.com>
2021-04-27 23:42:15 +0900

    'ファイルに書き込み保存し、分割し、表示する機能追加

--- board.php
+++ board.php
@@ -17,23 +17,50 @@
 // * 現在日時の出力方法を学ぶ
 // 
 
+// １）入力フォームと、それを受け取る準備：日付のみ自動で、他は入力値で。
+// ２）まずは、「投稿」→「表示」を実装　
+// ※1時間ではここまでしか出来なかった。スペルミスで時間無駄にしたのでスペル注意！
+// ３）一度投稿した名前をセッションに保存して自動反映を実装
+// ４）必須項目の投稿文を必須設定に。
+// ５）名前のMAX文字数設定
+// ６）投稿本文のMAX文字数設定
+// ７）本文をテキストファイルに保存（どうやるのか？独習PHPで復習して試す)
+// ━━━ここまでが最低限の仕様━ここまでを4月中に実装する━━━
+
 
 session_start();
 function h($str) {
 	return htmlspecialchars($str,ENT_QUOTES);
 }
-$sub_u_name="";
 if(!empty($_SESSION["u_name"])) {
     $sub_u_name = h($_SESSION["u_name"]);
+}else{
+    $sub_u_name="";
 }
 $sub_time="";
 if(!empty($_SESSION["sub_time"])){
     $sub_time= $_SESSION["sub_time"];
-    // echo $sub_time;
 }
 $u_text="";
 if(!empty($_SESSION["u_text"])){
-    $u_text= $_SESSION["u_text"];
+    $u_text= h($_SESSION["u_text"]);
+}
+
+define('FILENAME','./message.txt');
+$message_array=[];
+if($file_handle = fopen(FILENAME,'r')) {
+    // r＝＞ファイルを読み込みモードで開く
+    while($data= fgets($file_handle)) {
+        $split_data = preg_split('/\'/',$data);
+        $message = [
+            "view_name" => $split_data[1],
+            "message" => $split_data[3],
+            "post_date" => $split_data[5],
+        ];
+        array_unshift($message_array,$message);  
+    }
+    // ファイル閉じる
+    fclose($file_handle);
 }
 
 ;?>
@@ -43,28 +70,51 @@ if(!empty($_SESSION["u_text"])){
 <head>
     <meta charset="UTF-8">
     <title>LEVEL2：簡易掲示板</title>
+    <style>
+    article{
+        background-color: coral;
+        margin: 5px;
+        padding: 5px 20px;
+        border-radius:5px;
+    }
+    .p_date{
+        font-size: 13px;
+    }
+    </style>
 </head>
 <body>
 <h1>LEVEL2：簡易掲示板</h1>
 
 <section>
 <form action="exec_sub.php" method="post">
-    <p>名前：
-    <input type="text" name="u_name" value="">
+    <p><label for="u_name">名前：</label>
+    <input type="text" name="u_name" id="u_name" value="<?php echo $sub_u_name;?>">
     </p>
-    <p>投稿文：
-    <textarea type="text" name="u_text" value=""></textarea>
+    <p><label for="u_text">投稿文：</label>
+    <textarea type="text" name="u_text" id="u_text" value=""></textarea>
     </p>
     <input type="submit" name="sub" value="投稿する">
 </form>
 </section>
-
-<section>
+<hr>
+<!-- <section>
     <article>
-    <p>名前：<?php echo $sub_u_name;?></p>
-    <p>投稿日時：<?php echo $sub_time;?></p>
-    <p>投稿文：<?php echo $u_text;?></p>
+        <p>名前：<?php echo $sub_u_name;?></p>
+        <p>投稿日時：<?php echo $sub_time;?></p>
+        <p>投稿文：<?php echo $u_text;?></p>
     </article>
+</section> -->
+
+<section class="p2">
+    <?php if(!empty($message_array)): ?>
+    <?php foreach($message_array as $val): ?>
+    <article>
+        <p>名前：<?php echo $val["view_name"];?>
+        <span class="p_date">投稿日時：<?php echo $val["post_date"];?></span></p>
+        <p>投稿文：<?php echo $val["message"];?></p>
+    </article>
+    <?php endforeach; ?>
+    <?php endif; ?>
 </section>
     
 </body>
--- exec_sub.php
+++ exec_sub.php
@@ -5,8 +5,30 @@ if( empty($_POST["sub"]) ) {
 	exit();
 }
 
-session_start();
+define('FILENAME','./message.txt');
+date_default_timezone_set('Asia/Tokyo');
 
+if( $file_handle = fopen(FILENAME, "a")) {
+    // ファイルを開いて末尾に追加書き込みする準備
+
+    // 書き込み日時を取得
+    $now_date = date("Y年m月d日 H:i:s");
+    
+    // 書き込むデータ作成
+    if(!empty($_POST["u_name"])) {
+        $u_name = $_POST["u_name"];
+    }else{
+        $u_name = "NO NAME";
+    }
+    $data = "'".$u_name."','".$_POST["u_text"]."','".$now_date."'\n";
+    // 書き込む
+    fwrite($file_handle, $data);
+
+    // ファイルを閉じる
+    fclose( $file_handle );
+}
+
+session_start();
 if(!empty($_POST["u_name"])) {
     $_SESSION["u_name"] = $_POST["u_name"];
 }else{
@@ -14,6 +36,7 @@ if(!empty($_POST["u_name"])) {
 }
 $_SESSION["u_text"] = $_POST["u_text"];
 $_SESSION["sub_time"] = date("Y年m月d日 H:i:s");
+
 
 
 header("Location:board.php");
